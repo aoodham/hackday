730a58e6e1edd523ff21949d88b83c49f5c3c139
Anastasiia Balytska <anastasiia.balytska@eigentech.com>
2018-01-05 15:29:16 +0000

    add inputs

--- ideas.elm
+++ ideas.elm
@@ -1,5 +1,6 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
 
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
@@ -8,9 +9,10 @@ main =
 -- MODEL
 
 type alias Idea =
-  { name : String
+  { title : String
   , description: String
   , person: String
+  , status: String
   }
 
 type alias Model =
@@ -20,26 +22,51 @@ type alias Model =
 
 model : Model
 model = Model
-  (Idea "" "" "")
+  (Idea "" "" "" "")
   []
 
 
 -- UPDATE
 
-type Msg = Add | Reset
+type Msg = Add
+          | Reset
+          | UpdateTitle String
+          | UpdateDescription String
+          | UpdateStatus String
+          | UpdatePerson String
 
 update : Msg -> Model -> Model
-update msg model =
+update msg {new, saved} =
   case msg of
     Add ->
       Model
-        (Idea "" "" "")
-        (model.saved ++ [Idea "a" "b" "c"])
+        (Idea "" "" "" "")
+        (saved ++ [new])
 
     Reset ->
       Model
-        (Idea "" "" "")
+        (Idea "" "" "" "")
         []
+
+    UpdateTitle title ->
+      Model
+        {new | title = title}
+        saved
+
+    UpdateDescription description ->
+      Model
+        { new | description = description}
+        saved
+
+    UpdatePerson person ->
+      Model
+        { new | person = person}
+        saved
+
+    UpdateStatus status ->
+      Model
+        { new | status = status}
+        saved
 
 
 -- VIEW
@@ -47,11 +74,22 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Add ] [ text "+" ]
+    [ viewNewIdea model.new
+    , button [ onClick Add ] [ text "+" ]
     , div [] (List.map makeDiv model.saved)
     , button [ onClick Reset ] [ text "reset" ]
     ]
 
 makeDiv: Idea -> Html Msg
 makeDiv idea =
-  div [] [text (idea.name ++ " " ++ idea.description ++ " " ++ idea.person)]
+  div [] [text (idea.title ++ " " ++ idea.description ++ " " ++ idea.person ++ " " ++ idea.status)]
+
+viewNewIdea : Idea -> Html Msg
+viewNewIdea idea =
+   div []
+    [ input [ type_ "text", placeholder "Title", onInput UpdateTitle ] [text idea.title]
+    , input [ type_ "descr", placeholder "Description", onInput UpdateDescription ] [text idea.description]
+    , input [ type_ "person", placeholder "Person who proposed the idea", onInput UpdatePerson ] [text idea.person]
+    , input [ type_ "satus", placeholder "New", onInput UpdateStatus ] [text idea.status]
+    --, button [ onClick AddNew  ] [ text "Save" ]
+    ]
